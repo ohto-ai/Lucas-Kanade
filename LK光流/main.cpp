f3e5b85992b03ae42273c9c7c0480182d66e5839
THAT BOY <zhu.thatboy@outlook.com>
2020-10-21 09:09:18 +0800

    添加单目标跟踪和光流法目标跟踪

--- LK光流/main.cpp
+++ LK光流/main.cpp
@@ -4,68 +4,73 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 
-void saveMat(Mat& M, string s) {
-    s += ".txt";
-    FILE* pOut = fopen(s.c_str(), "w+");
-    for (int i = 0; i < M.rows; i++) {
-        for (int j = 0; j < M.cols; j++) {
-            fprintf(pOut, "%lf", M.at<double>(i, j));
-            if (j == M.cols - 1) fprintf(pOut, "\n");
-            else fprintf(pOut, " ");
-        }
-    }
-    fclose(pOut);
-}
 using namespace std;
 using namespace cv;
 int main()
 {
-    // 两帧图像
+    VideoCapture capture;
+
+    Mat frame, drawFrame;
     Mat prevFrame;
     Mat nextFrame;
-    resize(imread("mouse1.jpg", IMREAD_GRAYSCALE), prevFrame, Size(600, 600));
-    resize(imread("mouse2.jpg", IMREAD_GRAYSCALE), nextFrame, Size(600, 600));
-
-
+    capture.open(0);
+    capture >> frame;
     vector<Point2f> featurePrev;
+
+    int maxCoutFeature = 200;      // 最大特征点数量
+    double minDisFeature = 10;     // 特征点最小距离
+    double qLevelFeature = 0.01;   // 质量水平
+    vector<Point2f> featureNext;
     Mat featureStatus, featureError;
-    vector<Point2f> featurePyrLK;
 
-    int maxCoutFeature = 100;      // 最大特征点数量
-    double minDisFeature = 20;     // 特征点最小距离
-    double qLevelFeature = 0.05;   // 质量水平
-    // 提取特征点
-    goodFeaturesToTrack(prevFrame, featurePyrLK, maxCoutFeature, qLevelFeature, minDisFeature);
-
-    // 前帧图像标记特征点
-    for (int i = 0; i < featurePyrLK.size(); i++)
+    int refreshCount = 100;
+    while(1)
     {
-        circle(prevFrame, featurePyrLK[i], 3, Scalar(0), 1);
+        // 两帧图像
+        cvtColor(frame, prevFrame, COLOR_BGR2GRAY);
+        capture >> frame;
+        if (frame.empty())
+            break;
+        cvtColor(frame, nextFrame, COLOR_BGR2GRAY);
+        frame.copyTo(drawFrame);
+        if (frame.empty())
+            break;
+
+        auto countFeature = featureStatus.empty() ? 0 : std::count(featureStatus.begin<uchar>(), featureStatus.end<uchar>(), 1);
+
+        if (--refreshCount <= 0 || countFeature < 150)
+        {
+            refreshCount = 100;
+            goodFeaturesToTrack(prevFrame, featurePrev, maxCoutFeature, qLevelFeature, minDisFeature);
+        }
+        else
+            featurePrev = featureNext;
+        // 金字塔LK光流算法
+        calcOpticalFlowPyrLK(prevFrame, nextFrame, featurePrev, featureNext, featureStatus, featureError);
+
+        // 标记特征点位移
+        for (int i = 0; i < featureNext.size(); i++)
+        {
+            circle(drawFrame, featureNext[i], 3, Scalar(255), 1);
+            if (featureStatus.at<bool>(i))
+                line(drawFrame, featurePrev[i], featureNext[i], Scalar(1));
+        }
+        putText(drawFrame, "Features:" + std::to_string(countFeature) + "/" + std::to_string(featurePrev.size()), Point(0, 40), FONT_HERSHEY_SIMPLEX, 0.5, countFeature < 150 ? Scalar(0, 0, 255) : Scalar(0, 255, 255));
+        imshow("capture", drawFrame);
+        waitKey(20);
     }
+    //// 输出位移情况
+    //for (int i = 0; i < featureStatus.rows; i++)
+    //{
+    //    std::cout << "(" << featurePyrLK[i].x << "," << featurePyrLK[i].y << 
+    //        ")->(" << featurePrev[i].x << "," << featurePrev[i].y << ")" << std::endl;
+    //}
+    //std::cout << "prev pts:" << featurePyrLK.size() << std::endl;;
+    //std::cout << "next pts:" << featurePrev.size() << std::endl;
+    //std::cout << "status size：" << featureStatus.rows << std::endl;
 
-    // 金字塔LK光流算法
-    calcOpticalFlowPyrLK(prevFrame, nextFrame, featurePyrLK, featurePrev, featureStatus, featureError);
-
-    // 标记特征点位移
-    for (int i = 0; i < featurePrev.size(); i++)
-    {
-        circle(nextFrame, featurePrev[i], 3, Scalar(255), 1);
-        if(featureStatus.at<bool>(i))
-            line(nextFrame, featurePyrLK[i], featurePrev[i], Scalar(1));
-    }
-
-    // 输出位移情况
-    for (int i = 0; i < featureStatus.rows; i++)
-    {
-        std::cout << "(" << featurePyrLK[i].x << "," << featurePyrLK[i].y << 
-            ")->(" << featurePrev[i].x << "," << featurePrev[i].y << ")" << std::endl;
-    }
-    std::cout << "prev pts:" << featurePyrLK.size() << std::endl;;
-    std::cout << "next pts:" << featurePrev.size() << std::endl;
-    std::cout << "status size：" << featureStatus.rows << std::endl;
-
-    imshow("img1", prevFrame);
-    imshow("img2", nextFrame);
-    waitKey(0);
+    //imshow("img1", prevFrame);
+    //imshow("img2", nextFrame);
+    //waitKey(0);
     return 0;
 }
